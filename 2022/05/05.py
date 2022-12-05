075e1522d8df8b79919226ddaf1a34b0974c0b16
Eric A. Borisch <eborisch@gmail.com>
2022-12-05 13:17:45 -0600

    Day 5: Supply Stacks

---
+++ 2022/05/05.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+pat = re.compile(r'(\[(.)\]|   ) ?')
+move = re.compile(r'move (\d+) from (\d+) to (\d+)')
+
+dat = sys.stdin.readlines()
+blank = dat.index('\n')
+
+crates = dat[:blank-1]
+moves = dat[blank+1:]
+
+towers = [[] for n in range(len(crates[0])//4)]
+
+for y in crates:
+    t = [x[1] for x in pat.findall(y)]
+    for (twr, c) in zip(towers, t):
+        if c:
+            twr.insert(0,c)
+
+for y in moves:
+    x = move.match(y)
+    if not x:
+        break
+
+    count, src, dst = map(int, x.groups())
+    src -= 1
+    dst -= 1
+
+    towers[dst].extend(towers[src][-count:][::-1])
+    towers[src] = towers[src][:-count]
+
+for twr in towers:
+    print(twr.pop(), end='')
+
+print()
